d670b879127b9cc6d15a7b3809a2401b3d2580fa
WebApp07 <aminezouguig@proton.me>
2024-05-21 10:28:20 +0100

    Custom Error Middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 8000;
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 connectDB();
 
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
