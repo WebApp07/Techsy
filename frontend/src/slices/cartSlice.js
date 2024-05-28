3aac850e6bd0eafc383ebedeb466e4f44f3402f4
WebApp07 <aminezouguig@proton.me>
2024-05-28 15:35:16 +0100

    Cart Utils File

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartSlice";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
-
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -24,20 +21,7 @@ const cartSlice = createSlice({
       }
 
       // Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-      // Calculate shipping price
-      state.shippingPrice = addDecimals(state.itemPrice > 50 ? 0 : 3);
-      // Calculate tax price
-      state.taxPrice = addDecimals(Number((0.15 * state.itemPrice).toFixed(2)));
-      // Calculate total price
-      state.totalPrice =
-        Number(state.itemPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice).toFixed(2);
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
---
+++ frontend/src/utils/cartSlice.js
@@ -0,0 +1,22 @@
+export const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+export const updateCart = (state) => {
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  // Calculate shipping price
+  state.shippingPrice = addDecimals(state.itemPrice > 50 ? 0 : 3);
+  // Calculate tax price
+  state.taxPrice = addDecimals(Number((0.15 * state.itemPrice).toFixed(2)));
+  // Calculate total price
+  state.totalPrice =
+    Number(state.itemPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice).toFixed(2);
+
+  localStorage.setItem("cart", JSON.stringify(state));
+
+  return state;
+};
