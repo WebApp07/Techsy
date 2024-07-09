7bdb5cf1c3c58b791947ef66f0bb011b3ebcd530
WebApp07 <aminezouguig@proton.me>
2024-07-09 17:25:50 +0100

    pagination products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { Row, Col } from "react-bootstrap";
+import { pageNumber, useParams } from "react-router-dom";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
   return (
     <>
       {isLoading ? (
@@ -18,7 +20,7 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col sm={12} md={6} lg={4} xl={3}>
                 <Product key={product.id} product={product} />
               </Col>
